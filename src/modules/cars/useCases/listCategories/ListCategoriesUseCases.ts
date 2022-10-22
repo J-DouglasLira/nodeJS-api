98023251201756910a337128fc3fc134cd96044f
J-DouglasLira <jdouglas.developer@gmail.com>
2022-10-22 09:47:35 -0300

    refactor: list category

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCases.ts
@@ -0,0 +1,29 @@
+import { Category } from "../../model/Category";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+class ListCategoriesUseCase {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private categoriesRepository: ICategoriesRepository) { }
+  execute(): Category[] {
+    const categories = this.categoriesRepository.list();
+    return categories;
+  }
+}
+
+export { ListCategoriesUseCase };
+/* 
+class CreateCategoryUseCase {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private categoriesRepository: ICategoriesRepository) { }
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists!");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+*/
