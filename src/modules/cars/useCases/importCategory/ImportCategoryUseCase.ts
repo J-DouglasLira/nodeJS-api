a3aa7842c6a1f62561e997166908399feb8278a5
J-DouglasLira <jdouglas.developer@gmail.com>
2022-10-24 13:08:01 -0300

    feat: csvParse

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,20 @@
+import { parse as csvParse } from "csv-parse";
+import fs from "fs";
+
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+class ImportCategoryUseCase {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private categoriesRepository: ICategoriesRepository) { }
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => console.log(line));
+  }
+}
+
+export { ImportCategoryUseCase };
